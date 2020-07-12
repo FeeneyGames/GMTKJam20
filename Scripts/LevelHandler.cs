d32e8edee39a6cee4cc753d59cb2261d4e6f4de9
FeeneyGames <patrickfeeneygames@gmail.com>
2020-07-12 10:58:49 -0400

    Title and Level Transitions

---
+++ Scripts/LevelHandler.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public class LevelHandler : Control
+{
+    public int curLevel = -1;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if(Input.IsActionJustPressed("ui_cancel") ||
+           (curLevel == -1 && Input.IsActionJustPressed("ui_select")))
+            ChangeLevel(0);
+    }
+
+    public void ChangeLevel(int index)
+    {
+        curLevel = index;
+        Globals.gameOver = false;
+        switch(curLevel)
+        {
+            case -1:
+                GetTree().ChangeScene("res://Levels/Title.tscn");
+                break;
+            case 0:
+                GetTree().ChangeScene("res://Levels/Select.tscn");
+                break;
+            default:
+                GetTree().ChangeScene("res://Levels/Level" + index.ToString() + ".tscn");
+                break;
+        }
+    }
+}
--- UI/GameOver.cs
+++ UI/GameOver.cs
@@ -4,20 +4,28 @@ using System;
 public class GameOver : Label
 {
     private Timer timer;
+    private LevelHandler levelHandler;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         timer = GetNode<Timer>("../Timer");
+        levelHandler = GetNode<LevelHandler>("/root/LevelHandler");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        if(timer.time >= 29.9999)
-            Text = "BABYSAT FOR 30 SECONDS\nPress Space to Advance to the Next Level";
         if(Globals.gameOver)
         {
+            if(timer.time >= 29.9999)
+            {
+                Text = "BABYSAT FOR 30 SECONDS\nPress Space to Advance to the Next Level";
+                if(Input.IsActionJustPressed("ui_select"))
+                {
+                    levelHandler.ChangeLevel(levelHandler.curLevel + 1);
+                }
+            }
             Show();
             if(Input.IsActionJustPressed("ui_restart"))
             {
